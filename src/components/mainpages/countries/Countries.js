08f3ee900e782b7fd033731840f89ef062ef3c8d
hoangduylq <duy.luong@zodinet.tech>
2021-11-12 00:13:43 +0700

    fix search not found & addresponsive

--- src/components/mainpages/countries/Countries.js
+++ src/components/mainpages/countries/Countries.js
@@ -17,58 +17,35 @@ function Countries() {
 
   return (
     <div className='countries row'>
-      {countries.map((country, index) => (
-        <div className=' col c-3' key={index}>
-          <div className='country'>
-            <div className='country__img'>
-              <img src={country.flags.svg} alt='' />
-            </div>
-            <div className='country__info'>
-              <h2 className='country__info__name'>{country.name}</h2>
+      {!countries.status ? (
+        countries.map((country, index) => (
+          <div className=' col c-12 l-3 m-6' key={index}>
+            <div className='country'>
+              <div className='country__img'>
+                <img src={country.flags.svg} alt='' />
+              </div>
+              <div className='country__info'>
+                <h2 className='country__info__name'>{country.name}</h2>
 
-              <div>
-                <h6>Population:</h6>
-                <span>{getFormattedNumber(country.population)}</span>
-              </div>
-              <div>
-                <h6>Region:</h6>
-                <span>{country.region}</span>
-              </div>
-              <div>
-                <h6>Captital:</h6>
-                <span>{country.capital}</span>
+                <div>
+                  <h6>Population:</h6>
+                  <span>{getFormattedNumber(country.population)}</span>
+                </div>
+                <div>
+                  <h6>Region:</h6>
+                  <span>{country.region}</span>
+                </div>
+                <div>
+                  <h6>Captital:</h6>
+                  <span>{country.capital}</span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
-
-      {/* <div className=' col c-3'>
-        <div className='country'>
-          <div className='country__img'>
-            <img
-              src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/338px-Flag_of_Vietnam.svg.png'
-              alt=''
-            />
-          </div>
-          <div className='country__info'>
-            <h2 className='country__info__name'>Viet Nam</h2>
-
-            <div>
-              <h6>Population:</h6>
-              <span>81,770,900</span>
-            </div>
-            <div>
-              <h6>Region:</h6>
-              <span>Asian</span>
-            </div>
-            <div>
-              <h6>Captital:</h6>
-              <span>Ha Noi</span>
-            </div>
-          </div>
-        </div>
-      </div> */}
+        ))
+      ) : (
+        <h1 className='not-found'>Not Found</h1>
+      )}
     </div>
   );
 }
